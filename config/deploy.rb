e20829751cd6d7906722da078b7f47c5403a0acb
Daniel Park <menoelpis@gmail.com>
2016-08-27 18:15:29 +0900

    Modified and Fixed Somethings

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.6.1'
 
 set :application, 'digitaloceantest'
-set :repo_url, 'git@example.com:menoelpis/digitaloceantest.git'
+set :repo_url, 'git@github.com:menoelpis/digitaloceantest.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
