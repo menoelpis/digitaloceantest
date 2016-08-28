6887e60e79f44b0add60c5d131645b62003e20e9
Daniel Park <menoelpis@gmail.com>
2016-08-28 16:23:42 +0900

    Modified Deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,9 +34,10 @@ set :deploy_to, '/home/rails/digitaloceantest'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+set :bundle_binstubs, nil
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
 
