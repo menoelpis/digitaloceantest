8c81723b7de2d7da538369dc482e9559893faf67
Daniel Park <menoelpis@gmail.com>
2016-08-30 09:14:54 +0900

    Created Sessions Controller with Login Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  get 'signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
