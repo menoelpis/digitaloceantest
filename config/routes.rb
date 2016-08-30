5823fb8ea6ff806a35f76df4a4802c6179b7b977
Daniel Park <menoelpis@gmail.com>
2016-08-30 09:56:33 +0900

    Created Users Login Test with Flash.now Method

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get 'signup', to: 'users#new'
   get '/login', to: 'sessions#new'
-  get '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
